57010426804b64ae4e588067f9cb061611187b9c
Dusan Sotirov <dule.sotirov99@gmail.xom>
2022-12-09 18:41:24 +0100

    Dodavanje web api-a
    Dodavanje web api projekta i konfigurisanje .net7 verzije

---
+++ TaF_API/TaF_API/Program.cs
@@ -0,0 +1,102 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Neo4jClient;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TaF_WebAPI", Version = "v1" });
+
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securitySchema);
+
+    var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+    c.AddSecurityRequirement(securityRequirement);
+
+});
+
+
+var graphClient = new GraphClient(new Uri(
+                builder.Configuration.GetSection("Neo4jConnectionSettings:Server").Value),
+                builder.Configuration.GetSection("Neo4jConnectionSettings:User").Value,
+                builder.Configuration.GetSection("Neo4jConnectionSettings:Password").Value)
+{
+    DefaultDatabase = "taf"
+};
+
+graphClient.ConnectAsync();
+
+builder.Services.AddSingleton<IGraphClient, GraphClient>(_ => graphClient);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Jwt";
+    options.DefaultChallengeScheme = "Jwt";
+}).AddJwtBearer("Jwt", options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Lorem ipsum dolor sit amet, consectetur adipiscing elit")),
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.FromMinutes(5) //5 minute tolerance for the expiration date
+    };
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaF_WebAPI v1"));
+}
+
+app.UseCors(configurePolicy =>
+                configurePolicy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
